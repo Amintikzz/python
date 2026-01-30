f3000b709becb07547a8cbcb6eb0fe95a3378e61
Amintikzz <damiramir2502@gmail.com>
2026-01-30 09:46:29 +0500

    Update gptМонстр.py

--- gptМонстр.py
+++ gptМонстр.py
@@ -7,7 +7,7 @@ import speech_recognition as sr
 while True:
     input()
 
-    client = OpenAI(api_key="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")  # вставь свой ключ сюда
+    client = OpenAI(api_key="")  # вставь свой ключ сюда
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         audio = recognizer.listen(source)
@@ -37,3 +37,4 @@ while True:
         tts.save("звук.mp3")
         music=pyglet.resource.media("звук.mp3")
         music.play()
+
