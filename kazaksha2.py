78e70bd959dcf07c410ee94bd410f57f5ef9ea8f
Amintikzz <damiramir2502@gmail.com>
2026-01-30 09:47:24 +0500

    Update kazaksha2.py

--- kazaksha2.py
+++ kazaksha2.py
@@ -1,9 +1,10 @@
 import requests
 
 f=open('kazaksha.txt', 'w')
-API_KEY = "AIzaSyAqmYaKGX8zhH0BQtNcTxbx62IJO6ZIy9w"
+API_KEY = ""
 url = f"https://texttospeech.googleapis.com/v1/voices?key={API_KEY}"
 
 response = requests.get(url)
 f.write(str(response.json()))
 f.close()
+
